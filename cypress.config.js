dc98b5ab402cb26bf537e19136af9daf93bc97f7
darko <darko@contentinsights.com>
2024-10-03 19:28:01 +0200

    changed viewportHeight to correct size

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.holycode.com/",
     viewportWidth: 1920, 
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
       
     },
